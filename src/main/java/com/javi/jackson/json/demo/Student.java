b2b923d62ea2da4e332a49eb1430508dd92a8c54
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2021-01-24 16:39:10 +0100

    added new json property.
    Added anotation in Student to ignore unknown properties like the new one I just added in the json file.

--- src/main/java/com/javi/jackson/json/demo/Student.java
+++ src/main/java/com/javi/jackson/json/demo/Student.java
@@ -1,5 +1,8 @@
 package com.javi.jackson.json.demo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown=true)
 public class Student {
 	
 	private int id;
