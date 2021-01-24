1996f7263438604bda5274851a1fab634c245e2c
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2021-01-24 14:02:04 +0100

    created main driver app.
    created object mapper.
    
    read JSON file and map/convert to java POJO
    
    printed firstname and lastname of the student.

---
+++ src/main/java/com/javi/jackson/json/demo/Driver.java
@@ -0,0 +1,32 @@
+package com.javi.jackson.json.demo;
+
+import java.io.File;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			//create object mapper
+			ObjectMapper mapper = new ObjectMapper();
+			
+			//read JSON file and map/convert to java POJO:
+			//data/sample-lite.json
+			
+			Student theStudent = 
+					mapper.readValue(new File("data/sample-lite.json"), Student.class);
+			
+			//print first name and last name 
+			System.out.println("First Name: " + theStudent.getFirstName());
+			System.out.println("Last Name: " + theStudent.getLastName());
+			
+		} catch(Exception exc) {
+			
+		}
+
+	}
+
+}
